baf52eba6c21155e008d3f66bcd3a2dc2ad9bcac
Miguel García <miguelgs@Miguels-iMac.local>
2018-10-03 19:28:54 -0500

    home projects

--- wp-content/themes/ha-wordpress/page-home.php
+++ wp-content/themes/ha-wordpress/page-home.php
@@ -44,6 +44,17 @@
         ?>
 
         <? the_title() ?>
+        <? if (have_rows('description')): ?>
+        <? while (have_rows('description')) : the_row(); ?>
+        <? endwhile ?>
+        <? endif ?>
+
+        <? $image = wp_get_attachment_image_src(get_sub_field('image'), 'full') ?>
+        <div class="image image-display" style="background-image:url(<?= $image[0] ?>)">
+          <p class="caption">
+            Lorem ipsum
+          </p>
+        </div>
 
         <?
           wp_reset_postdata();
