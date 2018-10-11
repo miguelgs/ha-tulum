e870a278c272e734dd37b0cb16c99f54c648a1c9
Miguel García <miguelgs@Miguels-iMac.local>
2018-10-11 12:36:58 -0500

    terms & privacy page

---
+++ wp-content/themes/ha-wordpress/page-terms.php
@@ -0,0 +1,24 @@
+<? /* Template Name: Terms & Privacy */ ?>
+
+<!doctype html>
+<html>
+
+<head>
+  <? wp_head() ?>
+</head>
+
+<body>
+  <div id="terms-and-privacy" class="content-area">
+    <div class="screen-container">
+      <div class="content">
+        <p class="screen-title">
+          <? the_title() ?>
+        </p>
+        <? the_content() ?>
+      </div>
+    </div>
+  </div>
+  <? wp_footer() ?>
+</body>
+
+</html>
