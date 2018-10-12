839a61ac8cafda005e3637020ccf3596b8c90b48
Miguel García <miguelgs@Miguels-iMac.local>
2018-10-12 18:56:31 -0500

    responsive

--- wp-content/themes/ha-wordpress/common/js/bundle.js
+++ wp-content/themes/ha-wordpress/common/js/bundle.js
@@ -19924,9 +19924,7 @@ $('#about-us .collaborators .carousel').slick({
       breakpoint: 1024,
       settings: {
         slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
+        slidesToScroll: 3
       }
     },
     {
--- wp-content/themes/ha-wordpress/src/js/main.js
+++ wp-content/themes/ha-wordpress/src/js/main.js
@@ -54,9 +54,7 @@ $('#about-us .collaborators .carousel').slick({
       breakpoint: 1024,
       settings: {
         slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
+        slidesToScroll: 3
       }
     },
     {
