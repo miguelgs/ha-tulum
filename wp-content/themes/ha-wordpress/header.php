a5c354ef2377970b492ae4410bb51b065e3db180
Miguel García <miguelgs@Miguels-iMac.local>
2018-10-13 15:53:34 -0500

    responsive

--- wp-content/themes/ha-wordpress/header.php
+++ wp-content/themes/ha-wordpress/header.php
@@ -73,6 +73,6 @@
         <li><a href="#" rel="about-us"><?= pll_e( 'Nosotros' ) ?></a></li>
         <li><a href="#" rel="feature"><?= pll_e( 'Feature' ) ?></a></li>
         <li><a href="#" rel="contact-us"><?= pll_e( 'Contacto' ) ?></a></li>
-        <li class="language"><? pll_the_languages( array( 'hide_current' => 1 ) ) ?></li>
+        <? pll_the_languages( array( 'hide_current' => 1 ) ) ?>
       </ul>
     </div>
