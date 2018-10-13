7a756ec9e06e1c309783184194b347009b09046c
Miguel García <miguelgs@Miguels-iMac.local>
2018-10-13 13:34:19 -0500

    responsive

--- wp-content/themes/ha-wordpress/src/js/main.js
+++ wp-content/themes/ha-wordpress/src/js/main.js
@@ -84,14 +84,14 @@ $('#project-about .collaborators .carousel').slick({
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 3,
+        slidesToShow: 2,
         slidesToScroll: 2
       }
     },
     {
       breakpoint: 600,
       settings: {
-        slidesToShow: 2,
+        slidesToShow: 1,
         slidesToScroll: 1
       }
     }
